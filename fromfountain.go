d72c5d25c2ddcb0b7f7ca74e73e86e24dece0e11
R. S. Doiel <rsdoiel@gmail.com>
2017-12-31 21:51:32 -0800

    Quick Save

---
+++ fromfountain.go
@@ -0,0 +1,12 @@
+package fdx
+
+import (
+	"fmt"
+
+	// My Packages
+	"github.com/rsdoiel/fountain"
+)
+
+func (document *FinalDraft) FromFountain(screenplay *fountain.Fountain) error {
+	return fmt.Errorf("FromFountain() not implemented for fdx files")
+}
